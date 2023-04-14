e5bf26d2e00f5ddf6bc1103ba82a1373976fe840
IgorSelishev <0134613@gmail.com>
2023-04-14 11:33:16 +0300

    add TBot

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '6091847799:AAHD6M_T9yMEm82JTiT5LsIpxEXvmWMo-wU'
+
+exchange = {'евро' : 'EUR',
+       'доллар' : 'USD',
+       'юань': 'CNY',
+       'рубль' : 'RUB',
+       'биткоин': 'BTC',
+       'эфириум': 'ETH',
+              }
